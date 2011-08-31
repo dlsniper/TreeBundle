44fd3acde7fef785ec6df20dcb2874b3697f2136
Jacopo Jakuza Romei <jromei@gmail.com>
2011-08-31 16:21:30 +0200

    Minor code layout fix

--- Tests/test_source.php
+++ Tests/test_source.php
@@ -2,28 +2,18 @@
 
 require(__DIR__.'/../lib/vendor/jackalope/src/Jackalope/autoloader.php');
 
+require_once __DIR__.'/../src/PHPCRTree.php';
+
 $repository = Jackalope\RepositoryFactoryJackrabbit::getRepository(array(
     'jackalope.jackrabbit_uri' => 'http://localhost:8080/server'
 ));
 $credentials = new PHPCR\SimpleCredentials('jakuza', 'jakuza');
 $session = $repository->login($credentials, 'default');
 
-$root = isset($_REQUEST['root']) && $_REQUEST['root'] !== 'source' ? $_REQUEST['root'] : '/';
-$root = $session->getNode($root);
+$tree = new Ideato\TreeBundle\PHPCRTree($session);
 
-$tree = array();
+$path = isset($_REQUEST['root']) && $_REQUEST['root'] !== 'source' ? $_REQUEST['root'] : '/';
 
-foreach ($root->getNodes('*') as $name => $node) {
-    $child = array(
-        "text"  => $name,
-        "id"    => $node->getPath(),
-    );
-    if ($node->getNodes('*')) {
-        $child['hasChildren'] = true;
-    }
-    $tree[] = $child;
-}
-
-echo json_encode($tree);
+echo $tree->getJSON($path);
 
 ?>
