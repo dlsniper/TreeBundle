00cb939e98b99bf3048518e7f4ad7c8fe655f761
Jacopo Jakuza Romei <jromei@gmail.com>
2011-08-31 12:27:49 +0200

    Nodes browsing spike done.

--- Tests/test_source.php
+++ Tests/test_source.php
@@ -1,9 +1,29 @@
-[
-	{
-		"text": "Homepage yuk yuk",
-		"classes": "important",
-		"id":   "123",
-        "hasChildren": true
-        
-	}
-]
+<?php
+
+require(__DIR__.'/../lib/vendor/jackalope/src/Jackalope/autoloader.php');
+
+$repository = Jackalope\RepositoryFactoryJackrabbit::getRepository(array(
+    'jackalope.jackrabbit_uri' => 'http://localhost:8080/server'
+));
+$credentials = new PHPCR\SimpleCredentials('jakuza', 'jakuza');
+$session = $repository->login($credentials, 'default');
+
+$root = isset($_REQUEST['root']) && $_REQUEST['root'] !== 'source' ? $_REQUEST['root'] : '/';
+$root = $session->getNode($root);
+
+$tree = array();
+
+foreach ($root->getNodes('*') as $name => $node) {
+    $child = array(
+        "text"  => $name,
+        "id"    => $node->getPath(),
+    );
+    if ($node->getNodes('*')) {
+        $child['hasChildren'] = true;
+    }
+    $tree[] = $child;
+}
+
+echo json_encode($tree);
+
+?>
