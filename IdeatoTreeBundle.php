e5501b67fc36b68fd94490653c59a4a9af9f0ed6
Jacopo Jakuza Romei <jromei@gmail.com>
2011-10-17 18:28:57 +0200

    Namespace changed. Brought the bundle into CMF namespace.

--- IdeatoTreeBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ideato\TreeBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class IdeatoTreeBundle extends Bundle
-{
-}
---
+++ SymfonyCmfTreeBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Symfony\Cmf\Bundle\TreeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Jacopo Jakuza Romei <jromei@gmail.com>
+ */
+class SymfonyCmfTreeBundle extends Bundle
+{
+}
